867af8164c8d4d122a9b08bb0b7a645950887800
RamMarthi <161146370+RamMarthi9@users.noreply.github.com>
2024-12-29 07:57:45 +0530

    Delete run.py

--- run.py
+++
@@ -1,9 +0,0 @@
-from app import app  # Assuming your Flask app is defined in `app.py` or similar
-from flask_frozen import Freezer
-
-# Initialize the Freezer object
-freezer = Freezer(app)
-
-# This will generate static files
-if __name__ == "__main__":
-    freezer.freeze()  # Generates the static site files in the `build/` directory
